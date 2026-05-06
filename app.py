d85c3ff945f7141dc809712b1a7fe6d155a9c55d
varuhl <varuhl6362@gmail.com>
2026-05-06 13:09:08 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import qrvode
+from PIL import image
+# page title
+st.title("qr code generator")
+data=st.text_input("enter url")
+if st.button("generate qr"):
+  if data:
+    qr=qrcode.make(data)
+    qr.save("qr.png")
+    img=image.open("qr.png")
+    st.iamge(img,caption="generated qr code")
+with open("qr.png","rb")as f:
+  st.download_button("download qr",f,file_name=="qr.png")
+else:
+st.warning("please enter some text")
